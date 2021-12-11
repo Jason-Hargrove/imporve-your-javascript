c06f27e7e4901f238984d5ac07030b79924430de
Jason Hargrove <rijaha@gmail.com>
2021-12-11 15:11:13 -0600

    My first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Log everything.
+const log = console.log;
+// 7 Little-Known Techniques to Improve Your JavaScript
+
+// 1. Shorthand nullish filtering
+
+const groceries = ['Tea', null, 'Oat Milk', 'Carrots', undefined, 'Bread', ''];
+
+log(groceries);
