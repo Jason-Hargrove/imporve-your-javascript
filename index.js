8a6d3f20a6f69d9ed6ac9f774a0e8b0dcd2d5921
Jason Hargrove <rijaha@gmail.com>
2021-12-12 10:07:39 -0600

    Renaming variables during object destructuring.

--- index.js
+++ index.js
@@ -21,20 +21,20 @@ const log = console.log;
 
 
 // ===== 3. Combined array/object destructuring =====
-const people = [
-  {
-    name: "Lisa",
-    age: 20
-  },
-  {
-    name: "Pete",
-    age: 22
-  },
-  {
-    name: "Caroline",
-    age: 60
-  }
-];
+// const people = [
+//   {
+//     name: "Lisa",
+//     age: 20
+//   },
+//   {
+//     name: "Pete",
+//     age: 22
+//   },
+//   {
+//     name: "Caroline",
+//     age: 60
+//   }
+// ];
 
 // const [{age}] = people;
 // log(age);
@@ -44,8 +44,19 @@ const people = [
 
 
 // ===== 4. Ignoring values in array destructuring. =====
-const [, , caroline] = people;
+// const [, , caroline] = people;
 // log(caroline);
 
-const [, , {age}] = people;
-log(age);
+// const [, , {age}] = people;
+// log(age);
+
+
+// ===== 5. Renaming variables during object destructuring. =====
+const caroline = {
+  firstNm: 'Caroline',
+  ag: 27
+};
+log(caroline);
+
+const {firstNm: firstName, ag: age} = caroline;
+log(firstName, age);
