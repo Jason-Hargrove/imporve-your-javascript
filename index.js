0d1de093bebb5e05ec98c72c27ecc4778bedf138
Jason Hargrove <rijaha@gmail.com>
2021-12-12 09:34:26 -0600

    Formatting numbers with Numeric Seperators. Stage 4 proposal.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // Log everything.
 const log = console.log;
+
+// ====================================================
 // 7 Little-Known Techniques to Improve Your JavaScript
+// ====================================================
 
-// 1. Shorthand nullish filtering
+// ===== 1. Shorthand nullish filtering. =====
+// const groceries = ['Tea', null, 'Oat Milk', 'Carrots', undefined, 'Bread', ''];
+//
+// const cleanList = groceries.filter(Boolean);
+//
+// log(groceries);
+// log(cleanList);
 
-const groceries = ['Tea', null, 'Oat Milk', 'Carrots', undefined, 'Bread', ''];
 
-const cleanList = groceries.filter(Boolean);
-
-log(groceries);
-log(cleanList);
+// ===== 2. Formattin numbers with Numeric Seperators. =====
+// Stage 4 proposal.
+// const bigNumber = 1_000_000;
+// log(bigNumber); // 1000000
