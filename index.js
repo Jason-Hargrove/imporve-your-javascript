23cc5bd12d016bf94d214da9c2e5160ff63ab4ef
Jason Hargrove <rijaha@gmail.com>
2021-12-12 09:47:57 -0600

    Combined array/object destructuring.

--- index.js
+++ index.js
@@ -18,3 +18,29 @@ const log = console.log;
 // Stage 4 proposal.
 // const bigNumber = 1_000_000;
 // log(bigNumber); // 1000000
+
+
+// ===== 3. Combined array/object destructuring =====
+const people = [
+  {
+    name: "Lisa",
+    age: 20
+  },
+  {
+    name: "Pete",
+    age: 22
+  },
+  {
+    name: "Caroline",
+    age: 60
+  }
+];
+
+// const [{age}] = people;
+// log(age);
+//
+// const [{name}] = people;
+// log(name);
+
+
+// ===== 4. Ignoring values in array destructuring. =====
